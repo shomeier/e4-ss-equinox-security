2cbb6c7665987f4abd3ecf31aea563576bfdc877
Sascha Homeier <shomeier@meyle-mueller.de>
2014-12-07 22:13:07 +0100

    Introduced Login Handler Bundle

---
+++ org.eclipse.e4.security.demo.handler/src/org/eclipse/e4/security/demo/handler/LoginHandler.java
@@ -0,0 +1,12 @@
+package org.eclipse.e4.security.demo.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class LoginHandler
+{
+	@Execute
+	public void execute()
+	{
+
+	}
+}
--- org.eclipse.rap.e4.security.demo.ui/src/org/eclipse/rap/security/demo/ui/LoginPart.java
+++ org.eclipse.rap.e4.security.demo.ui/src/org/eclipse/rap/security/demo/ui/LoginPart.java
@@ -8,7 +8,8 @@ import javax.inject.Inject;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -24,25 +25,39 @@ public class LoginPart
 	@Inject
 	public LoginPart(Composite parent)
 	{
-		Composite composite_user = new Composite(parent, SWT.NONE);
+		// set grid layout to parent
+		parent.setLayout(new GridLayout());
 
-		// Display display = Display.getCurrent();
-		// composite.setBackground(display.getSystemColor(SWT.COLOR_BLUE));
+		Composite top = new Composite(parent, SWT.NONE);
 
-		composite_user.setLayout(new RowLayout(SWT.HORIZONTAL));
-		Label lbl_user = new Label(composite_user, SWT.NONE);
+		top.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = 5;
+		layout.marginWidth = 5;
+		layout.makeColumnsEqualWidth = true;// make each column have same width
+		layout.numColumns = 2; // number of columns
+		layout.verticalSpacing = 10;
+		top.setLayout(layout);
+
+		Label lbl_user = new Label(top, SWT.NONE);
 		lbl_user.setText("User:");
-		txt_user = new Text(composite_user, SWT.BORDER);
 
-		Composite composite_password = new Composite(parent, SWT.NONE);
-		composite_user.setLayout(new RowLayout(SWT.HORIZONTAL));
-		Label lbl_password = new Label(composite_password, SWT.NONE);
+		GridData gd_user = new GridData(SWT.FILL, SWT.CENTER, true, true);
+		txt_user = new Text(top, SWT.BORDER);
+		txt_user.setLayoutData(gd_user);
+
+		Label lbl_password = new Label(top, SWT.PASSWORD);
 		lbl_password.setText("Password:");
-		txt_password = new Text(composite_password, SWT.HORIZONTAL);
+		GridData gd_pw = new GridData(SWT.FILL, SWT.CENTER, true, true);
+		txt_password = new Text(top, SWT.BORDER);
+		txt_password.setLayoutData(gd_pw);
 
-		Button datumButton = new Button(composite_user, SWT.NONE);
-		datumButton.setText("Datum");
-		datumButton.addSelectionListener(new SelectionAdapter()
+		GridData gd_login = new GridData(SWT.FILL, SWT.CENTER, true, true);
+		gd_login.horizontalSpan = 2;
+		Button btn_login = new Button(top, SWT.NONE);
+		btn_login.setText("Login");
+		btn_login.setLayoutData(gd_login);
+		btn_login.addSelectionListener(new SelectionAdapter()
 		{
 			@Override
 			public void widgetSelected(SelectionEvent e)
@@ -50,17 +65,5 @@ public class LoginPart
 				txt_user.setText(new SimpleDateFormat("dd.MM.yyyy").format(new Date()));
 			}
 		});
-
-		Button zeitButton = new Button(composite_user, SWT.PUSH);
-		zeitButton.setText("Zeit");
-		zeitButton.addSelectionListener(new SelectionAdapter()
-		{
-			@Override
-			public void widgetSelected(SelectionEvent e)
-			{
-				txt_user.setText(new SimpleDateFormat("HH:mm:ss").format(new Date()));
-			}
-		});
-
 	}
 }
